0670f82bf5fcfec051e817f28a8bd979b3b832f9
Walter Rodrigues Junior <walter.junior00@pm.pr.gov.br>
2024-08-20 13:36:27 -0300

    atualizando

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,45 +1,12 @@
+// Exemplo de um Sidebar básico ou apenas remova o arquivo `Sidebar.js` se não for necessário.
 import React from 'react';
 
 const Sidebar = () => {
   return (
-    <aside className="main-sidebar sidebar-dark-primary elevation-4">
-      {/* Brand Logo */}
-      <a href="index3.html" className="brand-link">
-        <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" className="brand-image img-circle elevation-3" style={{ opacity: '.8' }} />
-        <span className="brand-text font-weight-light">AdminLTE 3</span>
-      </a>
-
-      {/* Sidebar */}
-      <div className="sidebar">
-        {/* Sidebar user panel (optional) */}
-        <div className="user-panel mt-3 pb-3 mb-3 d-flex">
-          <div className="image">
-            <img src="dist/img/user2-160x160.jpg" className="img-circle elevation-2" alt="User Image" />
-          </div>
-          <div className="info">
-            <a href="#" className="d-block">User Name</a>
-          </div>
-        </div>
-
-        {/* Sidebar Menu */}
-        <nav className="mt-2">
-          <ul className="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-            <li className="nav-item">
-              <a href="/home" className="nav-link">
-                <i className="nav-icon fas fa-home"></i>
-                <p>
-                  Home
-                </p>
-              </a>
-            </li>
-            {/* Add more links as necessary */}
-          </ul>
-        </nav>
-        {/* /.sidebar-menu */}
-      </div>
-      {/* /.sidebar */}
-    </aside>
+    <div>
+      {/* Conteúdo básico do Sidebar */}
+    </div>
   );
-}
+};
 
 export default Sidebar;
